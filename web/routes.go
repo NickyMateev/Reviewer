346e1feb3d5d695f45ac2bfebdd4106ddc4c160b
NickyMateev <bgdunkers@gmail.com>
2019-02-16 18:52:47 +0200

    Address GoReport alerts

--- web/routes.go
+++ web/routes.go
@@ -1,14 +1,14 @@
 package web
 
 const (
-	apiVersion      = "v1"
+	apiVersion = "v1"
 
 	// ProjectsURL is the URL path to manage Projects
-	ProjectsURL     = "/" + apiVersion + "/projects"
+	ProjectsURL = "/" + apiVersion + "/projects"
 
 	// PullRequestsURL is the URL path to manage Pull Requests
 	PullRequestsURL = "/" + apiVersion + "/pull_requests"
 
 	// UsersURL is the URL path to manage Users
-	UsersURL        = "/" + apiVersion + "/users"
+	UsersURL = "/" + apiVersion + "/users"
 )
